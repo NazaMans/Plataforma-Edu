ecd687ea4b2db7b92d62fdac2c04476dc7d3da83
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-06-28 19:39:20 -0300

    Se agrego RETURNING  a las sentencias de cada modulo

--- backend/src/modules/cursos/cursos.repisitory.js
+++ backend/src/modules/cursos/cursos.repisitory.js
@@ -2,26 +2,26 @@ import pool from "../../config/db.js";
 
 //Crear nuevo curso
 export const crearCurso = async (nombre) => {
-    const query = await pool.query("INSERT INTO cursos (nombre) VALUES ($1)", [nombre]);
+    const query = await pool.query("INSERT INTO cursos (nombre) VALUES ($1) RETURNING id, nombre, estado", [nombre]);
     return query.rows[0];
 };
 
 //Inscribir usuario a curso
 
 export const inscribirUsuarioACurso = async (id_curso, id_usuario) => {
-    const query = await pool.query("INSERT INTO cursos_usuarios (id_curso, id_usuario) VALUES ($1, $2)", [id_curso, id_usuario]);
+    const query = await pool.query("INSERT INTO cursos_usuarios (id_curso, id_usuario) VALUES ($1, $2) RETURNING id_curso, id_usuario", [id_curso, id_usuario]);
     return query.rows[0];
 };
 
 //Editar curso
 export const editarCurso = async (id_curso, nombre, estado) => {
-    const query = await pool.query("UPDATE cursos SET nombre = $1, estado = $2 WHERE id = $3", [nombre, estado, id_curso]);
+    const query = await pool.query("UPDATE cursos SET nombre = $1, estado = $2 WHERE id = $3 RETURNING id, nombre, estado", [nombre, estado, id_curso]);
     return query.rows[0];
 };
 
 //Desinscribir usuario a curso
 export const desinscribirUsuarioACurso = async (id_curso, id_usuario) => {
-    const query = await pool.query("DELETE FROM cursos_usuarios WHERE id_curso = $1 AND id_usuario = $2", [id_curso, id_usuario]);
+    const query = await pool.query("DELETE FROM cursos_usuarios WHERE id_curso = $1 AND id_usuario = $2 RETURNING id_curso, id_usuario", [id_curso, id_usuario]);
     return query.rows[0];
 };
 
@@ -65,7 +65,7 @@ export const getCursoEditByName = async (nombre, id_curso) => {
 };
 
 export const eliminarCurso = async (id_curso) => {
-    const query = await pool.query("UPDATE cursos SET estado = false WHERE id = $1", [id_curso]);
+    const query = await pool.query("UPDATE cursos SET estado = false WHERE id = $1 RETURNING id, estado", [id_curso]);
     return query.rows[0];
 };
 
--- backend/src/modules/examenes/examenes.repository.js
+++ backend/src/modules/examenes/examenes.repository.js
@@ -4,6 +4,7 @@ export const crearExamen = async (id_curso, nombre, actividad, resolucion) => {
     const query = await pool.query(`
     INSERT INTO examenes (curso_id, nombre, actividad, resolucion)
     VALUES ($1, $2, $3, $4)
+    RETURNING id, curso_id, nombre, actividad, resolucion, estado
     `, [id_curso, nombre, actividad, resolucion]);
     return query.rows[0];
 };
@@ -15,7 +16,8 @@ export const getExamenesAdmin = async () => {
         e.id,
         e.nombre,
         e.actividad,
-        e.resolucion
+        e.resolucion,
+        e.estado
     FROM examenes e
     ORDER BY e.id ASC
     `);
@@ -42,7 +44,8 @@ export const getExamenById = async (id_examen) => {
         e.id,
         e.nombre,
         e.actividad,
-        e.resolucion
+        e.resolucion,
+        e.estado
     FROM examenes e
     WHERE e.id = $1 AND e.estado = true
     `, [id_examen]);
@@ -52,20 +55,20 @@ export const getExamenById = async (id_examen) => {
 //Editar examen
 export const editarExamen = async (id_examen, id_curso, nombre, actividad, resolucion) => {
     const query = await pool.query(`
-    UPDATE examenes SET curso_id = $1, nombre = $2, actividad = $3, resolucion = $4 WHERE id = $5
+    UPDATE examenes SET curso_id = $1, nombre = $2, actividad = $3, resolucion = $4 WHERE id = $5 RETURNING id, curso_id, nombre, actividad, resolucion, estado
     `, [id_curso, nombre, actividad, resolucion, id_examen]);
     return query.rows[0];
 };
 
 //Eliminar examen
 export const eliminarExamen = async (id_examen) => {
-    const query = await pool.query("UPDATE examenes SET estado = false WHERE id = $1", [id_examen]);
+    const query = await pool.query("UPDATE examenes SET estado = false WHERE id = $1 RETURNING id, estado", [id_examen]);
     return query.rows[0];
 };
 
 //Activar examen
 
 export const activarExamen = async (id_examen) => {
-    const query = await pool.query("UPDATE examenes SET estado = true WHERE id = $1", [id_examen]);
+    const query = await pool.query("UPDATE examenes SET estado = true WHERE id = $1 RETURNING id, estado", [id_examen]);
     return query.rows[0];
 }
