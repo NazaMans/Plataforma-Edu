a02b992b781f949f514800197610186c7f0a8897
Nazareno Exequiel Mansilla <mansillanaza@gmail.com>
2026-06-28 19:33:18 -0300

    Se agrega limite de conexiones a la base de datos en el pool

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,6 +6,9 @@ dotenv.config();
 const {Pool} = pg;
 const pool = new Pool({
     connectionString: process.env.DB_URI,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 export const testConection = async () => {
